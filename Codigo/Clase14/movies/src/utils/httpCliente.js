074e83c4c866d1a12cdcf301e0ce62ed28c70800
Bussomauro <bussomauro@gmail.com>
2023-04-22 08:43:14 -0300

    OK

--- Codigo/Clase14/movies/src/utils/httpCliente.js
+++ Codigo/Clase14/movies/src/utils/httpCliente.js
@@ -4,7 +4,7 @@ export const get = (path) => {
   return fetch(API + path, {
     headers: {
       Authorization:
-        "",
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYTJlMGE0MDNlNDNkZmI1OGVhYjJiYWY2YzQ1YWZmYiIsInN1YiI6IjY0MjYzNzEzOWNjNjdiMDBkMzUxMmEzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lPSQ5SV3oojf3EsS5ir3W1lKxVb7xQajDNOeoOkjbOY",
       "Content-Type": "application/json;chartset=utf-8",
     },
   }).then((results) => results.json());
